d2263cdccde7d64f42b150205244c915e2086b61
roto93 <roto93@yahoo.com.tw>
2021-06-18 14:43:15 +0800

    6/18 update nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './reset.css'
 import './App.css';
-import React from 'react'
+import React, { useState } from 'react'
 import * as SC from './components/StyledComponents'
 import * as SVG from './images/SVG'
 import BG from './images/BG.png'
 
 function App() {
+  const [showNav, setShowNav] = useState(false);
+
+
   return (
     <div className="App">
       <div className="banner">
+        <header>
+          <div className="observatory">
+            <SVG.SvgObservatory />
+          </div>
+          <div className="burger"
+            onClick={() => { setShowNav(!showNav) }}>
+            <SVG.SvgMenu />
+          </div>
+        </header>
+        <div className={showNav ? "menu active" : "menu"}>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Links</li>
+            <li>Archive</li>
+          </ul>
+        </div>
         <div className="title-box">
           <ul className="nav">
             <li>Home</li>
@@ -19,19 +39,18 @@ function App() {
             <li>Links</li>
             <li>Archive</li>
           </ul>
-          <header>
-            <div className="txt">
-              <span>
-                <h1>T</h1><h2>aiwan</h2>
-              </span>
-              <span>
-                <h1>A</h1><h2>stronomy</h2>
-              </span>
-              <span>
-                <h1>N</h1><h2>etwork</h2>
-              </span>
+          <div className="txt">
+
+            <div>
+              <h1>T</h1><h2>aiwan</h2>
             </div>
-          </header>
+            <div>
+              <h1>A</h1><h2>stronomy</h2>
+            </div>
+            <div>
+              <h1>N</h1><h2>etwork</h2>
+            </div>
+          </div>
           <div className="arrow">
             <SVG.SvgArrow />
           </div>
--- src/images/SVG.jsx
+++ src/images/SVG.jsx
@@ -58,3 +58,17 @@ export function SvgArrow(props) {
         </svg>
     );
 }
+
+export function SvgMenu(props) {
+    return (
+        <svg
+            height="1em"
+            viewBox="0 -53 384 384"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path fill="currentColor" d="M368 154.668H16c-8.832 0-16-7.168-16-16s7.168-16 16-16h352c8.832 0 16 7.168 16 16s-7.168 16-16 16zm0 0M368 32H16C7.168 32 0 24.832 0 16S7.168 0 16 0h352c8.832 0 16 7.168 16 16s-7.168 16-16 16zm0 0M368 277.332H16c-8.832 0-16-7.168-16-16s7.168-16 16-16h352c8.832 0 16 7.168 16 16s-7.168 16-16 16zm0 0" />
+        </svg>
+    );
+}
